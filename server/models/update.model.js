1f983516c0ce31dc936238c667fb0423104bddfe
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-05 17:20:36 +0100

    removed useless spaces

--- server/models/update.model.js
+++ server/models/update.model.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const updateSchema = new Schema({
-    template: [{ 
-        type: Schema.Types.ObjectId, 
-        ref: 'Template', 
+    template: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Template',
         required: true
     }],
-    user: [{ 
-        type: Schema.Types.ObjectId, 
-        ref: 'User', 
+    user: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     }],
 }, {
