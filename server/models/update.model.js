01bdbecf3da4297aba1748b551978d1c1433b824
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-05 17:18:30 +0100

    update model created

---
+++ server/models/update.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const updateSchema = new Schema({
+    template: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'Template', 
+        required: true
+    }],
+    user: [{ 
+        type: Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true
+    }],
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const Update = mongoose.model('Update', updateSchema);
+module.exports = Update;
