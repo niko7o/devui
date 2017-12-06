8a9314ec9abcbf92113d0faf7aaf893f755ce04b
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-06 13:47:39 +0100

    started vote logic for templates

--- server/routes/template.js
+++ server/routes/template.js
@@ -1,61 +1,31 @@
 const express = require('express');
 const passport = require('passport');
+
 const User = require('../models/user.model');
 const Template = require('../models/template.model');
+
 const templateRoutes = express.Router();
 
-templateRoutes.post('/:id/addvote', (req, res, next) => {
-  
+templateRoutes.post("/:id/addvote", (req, res, next) => {
+  const templateId = req.params.id;    
+  const votes = req.body.votes;
+  const newVoteCount = votes++;
+
+  Template.findByIdAndUpdate(templateId,
+    { "$push": { "votes": newVoteCount } }, { new: true })
+    .then(o => res.json(o))
+    .catch(err => res.json(err));
 });
 
-// templateRoutes.post('/new', (req, res, next) => {
-//   const {
-//     title,
-//     description,
-//     images,
-//     developers,
-//     updates,
-//     creator,
-//     votes
-//   } = req.body;
+templateRoutes.post("/:id/subvote", (req, res, next) => {
+  const templateId = req.params.id;    
+  const votes = req.body.votes;
+  const newVoteCount = votes--;
 
-//   if (!title || !description) {
-//     res.status(400).json({ message: 'Provide a template title and description' });
-//     return;
-//   }
-
-//   Template.findOne({ title }, '_id')
-//   .then(template => {
-//     if (template) {
-//       res.status(400).json({ message: 'A template with this name already exists' });
-//       return;
-//     }
-//     const theTemplate = new Template({
-//       title,
-//       description,
-//       images,
-//       developers,
-//       updates,
-//       creator,
-//       votes
-//     });
-//     return theTemplate.save();
-//   })
-//   .then(newTemplate => {
-//     console.log(`creating new template: ${newTemplate}`);
-//     req.login(newTemplate, (err) => {
-//       if (err) {
-//         console.log(err);
-//         res.status(500).json({ message: 'Something went wrong' });
-//         return;
-//       }
-//       res.status(200).json(req.user);
-//     });
-//   })
-//   .catch(e => {
-//       console.log(e)
-//       res.status(500).json({ message: 'Something went wrong' });
-//   });
-// });
+  Template.findByIdAndUpdate(templateId,
+    { "$push": { "votes": newVoteCount } }, { new: true })
+    .then(o => res.json(o))
+    .catch(err => res.json(err));
+});
 
 module.exports = templateRoutes;
