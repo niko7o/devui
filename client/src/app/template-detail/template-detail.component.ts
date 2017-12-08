0f5baea131f61e16f3f9bddbc5f1e64b67e70eb1
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-08 17:56:10 +0100

    beer time

--- client/src/app/template-detail/template-detail.component.ts
+++ client/src/app/template-detail/template-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TemplateService } from '../services/template.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-template-detail',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDetailComponent implements OnInit {
 
-  constructor() { }
+  template: any;
 
-  ngOnInit() {
+  constructor(
+    public router: Router,
+    public tempServ: TemplateService,
+    public route: ActivatedRoute) {
+    route.params.subscribe(params => {
+      this.tempServ.getTemplateByID(params['id']).subscribe(template => this.template = template);
+    });
+  }
+
+  ngOnInit() {}
+
+  deletePhone(id) {
+    this.tempServ.deleteTemplate(id).subscribe(() => {
+      this.router.navigate(['templates']);
+    });
   }
 
 }
