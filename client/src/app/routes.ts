4fce09aa26f0c414261b31246d58bf4691dd6edd
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-08 12:26:50 +0100

    cambio de rutas

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TemplateListComponent } from './template-list/template-list.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'templates', component: TemplateListComponent },
-    { path: 'signup', component: TemplateListComponent },
-    { path: 'login', component: TemplateListComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: SignupComponent },
     { path: '**', redirectTo: '' }
 ];
