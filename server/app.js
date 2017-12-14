004e692fdaf4767cccf921f3529e75272d5380cd
Nikolaas Verlee <nikotomad@gmail.com>
2017-12-14 16:32:36 +0100

    preparing final deploy

--- server/app.js
+++ server/app.js
@@ -75,9 +75,10 @@ app.use('/api/templates', api(require('./models/template.model')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next("/");
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  // next(err);
 });
 
 // app.use(function(req, res) {
